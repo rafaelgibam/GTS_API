e994fabce86f7b71e8bbf7c7f867ce8089b1c27a
Rafael Willames <rafael.w.silva@hotmail.com>
2020-07-23 23:37:04 -0300

    feat: primeiro commit

---
+++ src/main/java/br/com/gestaotrading/GtsApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestaotrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtsApplication.class, args);
+	}
+
+}
